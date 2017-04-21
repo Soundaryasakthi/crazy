050f380532eef85d7c495f7a5989cd365fe90459
Soundarya S <soundaryasakthi14@gmail.com>
2017-04-21 18:50:59 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int base,exp;
+double double int value=1;
+printf("Enter Number\n");
+scanf("%d%d",&base,&exp);
+while(exp!=0)
+{
+value*=base;
+--exp;}
+printf("%d",value);
+return 0;
+}
